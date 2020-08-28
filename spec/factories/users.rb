265f27cf73fb9b12a33e198a321fed40766cda17
hide8811 <ikuyedih18@gmail.com>
2020-08-28 19:05:30 +0900

    Add: userのダミーデータを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :user do
+    employee_id         { Faker::Alphanumeric.unique.alphanumeric }
+    encrypted_password  { 'Test1234' }
+    last_name           { Gimei.last.kanji }
+    first_name          { Gimei.first.kanji }
+    last_name_kana      { Gimei.last.hiragana }
+    first_name_kana     { Gimei.first.hiragana }
   end
 end
