399acc7536e2b7abebdc2e7280d95529d95eb0de
hide8811 <ikuyedih18@gmail.com>
2020-09-05 02:03:47 +0900

    Add: capybara設定ファイルの追加

---
+++ spec/support/capybara.rb
@@ -0,0 +1,18 @@
+Capybara.register_driver :chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1680,1050')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :chrome_headless
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+end
