96a31500b5598a23c884027d9c998380dbb24a89
hide8811 <ikuyedih18@gmail.com>
2020-09-05 04:10:04 +0900

    Add: ログイン時のカラム許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[employee_id last_name first_name last_name_kana first_name_kana status])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:employee_id])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[employee_id last_name first_name last_name_kana first_name_kana status])
   end
 end
