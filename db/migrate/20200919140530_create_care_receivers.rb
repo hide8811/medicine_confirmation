00ded6ec2d5e30d9d6efec568e55301b63a4dcf2
hide8811 <ikuyedih18@gmail.com>
2020-09-20 00:18:19 +0900

    Add: [care_receiver] マイグレーションファイルの編集

--- db/migrate/20200919140530_create_care_receivers.rb
+++ db/migrate/20200919140530_create_care_receivers.rb
@@ -1,6 +1,12 @@
 class CreateCareReceivers < ActiveRecord::Migration[6.0]
   def change
     create_table :care_receivers do |t|
+      t.string  :last_name,        null: false
+      t.string  :first_name,       null: false
+      t.string  :last_name_kana,   null: false
+      t.string  :first_name_kana,  null: false
+      t.date    :birthday,         null: false
+      t.boolean :enroll,           null: false, default: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_120232) do
+ActiveRecord::Schema.define(version: 2020_09_19_140530) do
+
+  create_table "care_receivers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birthday", null: false
+    t.boolean "enroll", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "employee_id", default: "", null: false
