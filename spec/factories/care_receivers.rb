10920f1111b182fa0d1d4711389cef894e4dbe66
hide8811 <ikuyedih18@gmail.com>
2020-09-20 00:49:13 +0900

    Add: [care_receiver] テストのダミーデータを追加

--- spec/factories/care_receivers.rb
+++ spec/factories/care_receivers.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :care_receiver do
-    
+    last_name       { Gimei.last.kanji }
+    first_name      { Gimei.first.kanji }
+    last_name_kana  { Gimei.last.hiragana }
+    first_name_kana { Gimei.first.hiragana }
+    birthday        { Faker::Date.birthday(min_age: 65, max_age: 120) }
+    enroll          { true }
   end
 end
