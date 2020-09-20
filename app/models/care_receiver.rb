366c25da0c5ddeaea6b214be0446c6cf9e8e6fd7
hide8811 <ikuyedih18@gmail.com>
2020-09-20 20:07:46 +0900

    Add: [care_receiver] バリデーションの設定

--- app/models/care_receiver.rb
+++ app/models/care_receiver.rb
@@ -1,2 +1,11 @@
 class CareReceiver < ApplicationRecord
+  with_options presence: true do |p|
+    p.validates :last_name
+    p.validates :first_name
+    p.validates :last_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
+    p.validates :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
+    p.validates :birthday
+  end
+
+  validates :enroll, inclusion: { in: [true, false] }
 end
