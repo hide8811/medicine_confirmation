a81d69a5f9739e8e80f9d1d712322fd7f252e84f
hide8811 <ikuyedih18@gmail.com>
2020-09-22 13:13:11 +0900

    Add: [dosing_time] バリデーションの設定

--- app/models/dosing_time.rb
+++ app/models/dosing_time.rb
@@ -1,3 +1,9 @@
 class DosingTime < ApplicationRecord
   belongs_to :care_receiver
+
+  with_options presence: true do |p|
+    p.validates :time,      uniqueness: { case_sensitive: false, scope: :care_receiver_id }
+    p.validates :timeframe, uniqueness: { case_sensitive: false, scope: :care_receiver_id }
+    p.validates :care_receiver_id
+  end
 end
