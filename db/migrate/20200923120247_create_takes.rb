4cb033b7dada0428914dbf83394f7bf54436dac1
hide8811 <ikuyedih18@gmail.com>
2020-09-23 21:11:46 +0900

    Add: [take] マイグレーションファイルの編集

--- db/migrate/20200923120247_create_takes.rb
+++ db/migrate/20200923120247_create_takes.rb
@@ -1,11 +1,11 @@
 class CreateTakes < ActiveRecord::Migration[6.0]
   def change
     create_table :takes do |t|
-      t.boolean :execute
-      t.string :dosing_timeframe
-      t.time :dosing_time
-      t.references :user, null: false, foreign_key: true
-      t.references :care_receiver, null: false, foreign_key: true
+      t.boolean    :execute,          null: false, default: false
+      t.string     :dosing_timeframe, null: false
+      t.time       :dosing_time,      null: false
+      t.references :user,             null: false, foreign_key: true
+      t.references :care_receiver,    null: false, foreign_key: true
 
       t.timestamps
     end
