1cb3e9a13d92b44e31e72513e079d83dfe14fdae
hide8811 <ikuyedih18@gmail.com>
2020-09-23 22:39:18 +0900

    Add: [take] テストのダミーデータを追加

--- spec/factories/takes.rb
+++ spec/factories/takes.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :take do
-    execute { false }
-    dosing_timeframe { "MyString" }
-    dosing_time { "2020-09-23 12:02:47" }
-    user { nil }
-    care_receiver { nil }
+    execute          { false }
+    dosing_timeframe { '朝食後' }
+    dosing_time      { Faker::Time.between(from: Time.now - 1, to: Time.now, format: :short) }
+
+    user
+    care_receiver
   end
 end
