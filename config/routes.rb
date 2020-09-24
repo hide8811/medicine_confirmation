b48ee7c9cb5c761bb557decfeb95c6adb310dd6b
hide8811 <ikuyedih18@gmail.com>
2020-09-24 21:50:39 +0900

    Add: [care_receiver] controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'care_receivers/new'
+  get 'care_receivers/create'
+  get 'care_receivers/show'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_scope :user do
     get 'users/employee_uniquness', to: 'users/registrations#employee_uniquness'
