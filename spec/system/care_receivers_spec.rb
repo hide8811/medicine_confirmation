e6133fcb3f57297281041bb7a752443eeaf193f2
hide8811 <ikuyedih18@gmail.com>
2020-09-27 14:51:15 +0900

    Add: [care_receiver] リンクボタンのテストを追加

--- spec/system/care_receivers_spec.rb
+++ spec/system/care_receivers_spec.rb
@@ -2,8 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'CareReceivers', type: :system do
   before do
-    driven_by(:rack_test)
+    user = create(:user)
+    login_as(user, scope: :user)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe '新規登録' do
+    describe 'サイドメニュー' do
+      before do
+        visit root_path
+        click_on 'care_receiver 新規作成'
+      end
+
+      context '戻るボタンを押した時' do
+        it 'ホーム画面に戻ること' do
+          click_on '戻る'
+          expect(current_path).to eq root_path
+        end
+      end
+
+      context 'ホームボタンを押した時' do
+        it 'ホーム画面に戻ること' do
+          click_on 'ホーム'
+          expect(current_path).to eq root_path
+        end
+      end
+    end
+  end
 end
