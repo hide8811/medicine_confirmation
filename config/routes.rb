6d5d23aeb7aa1db080b613fc2909698a73d42bb7
hide8811 <ikuyedih18@gmail.com>
2020-09-27 14:36:34 +0900

    Improve: [care_receiver] ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'care_receivers/new'
-  get 'care_receivers/create'
-  get 'care_receivers/show'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_scope :user do
     get 'users/employee_uniquness', to: 'users/registrations#employee_uniquness'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'notes#index'
+  resources :care_receivers, only: %i[new create show]
 end
