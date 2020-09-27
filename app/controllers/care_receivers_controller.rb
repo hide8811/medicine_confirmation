98cc4ad69318bfb9f9bb75b6780c533ada37f2fb
hide8811 <ikuyedih18@gmail.com>
2020-09-27 17:38:54 +0900

    Add: [care_receiver] new・createアクションを編集

--- app/controllers/care_receivers_controller.rb
+++ app/controllers/care_receivers_controller.rb
@@ -2,9 +2,25 @@ class CareReceiversController < ApplicationController
   before_action :authenticate_user!
   protect_from_forgery prepend: true
 
-  def new; end
+  def new
+    @care_receiver = CareReceiver.new
+  end
 
-  def create; end
+  def create
+    @care_receiver = CareReceiver.new(care_receiver_params)
+
+    if @care_receiver.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def show; end
+
+  private
+
+  def care_receiver_params
+    params.require(:care_receiver).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, enroll: true)
+  end
 end
