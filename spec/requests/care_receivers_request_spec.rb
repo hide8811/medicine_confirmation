0b7bc65a6e85c7076466f53a8602c6f97dd3855c
hide8811 <ikuyedih18@gmail.com>
2020-09-27 16:50:00 +0900

    Add: [care_receiver] request specにログイン情報を追記

--- spec/requests/care_receivers_request_spec.rb
+++ spec/requests/care_receivers_request_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'CareReceivers', type: :request do
+  before do
+    user = create(:user)
+    login_as(user, scope: :user)
+  end
+
   describe 'GET /new' do
     it 'returns http success' do
       get '/care_receivers/new'
