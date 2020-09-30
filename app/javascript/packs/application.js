bd5183f24208a90813c2748ec03daeaa69976c19
hide8811 <ikuyedih18@gmail.com>
2020-09-30 21:08:37 +0900

    Add: [care_receiver] 新規登録後フラッシュメッセージを消すjQueryを追加
    メッセージ確認後は必要ないため

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("jquery")
 require("users/form.js")
 require("care_receivers/form.js")
+require("notes/index.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
