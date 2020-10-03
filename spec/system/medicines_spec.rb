ca9ffaae10d03f355344bd64c769a013c363ee3a
hide8811 <ikuyedih18@gmail.com>
2020-10-03 14:43:37 +0900

    Add: [medicine] 一覧表示のテストを追加

--- spec/system/medicines_spec.rb
+++ spec/system/medicines_spec.rb
@@ -26,4 +26,44 @@ RSpec.describe 'Medicines', type: :system do
       end
     end
   end
+
+  describe '薬一覧表示' do
+    it '薬の一覧が表示されること' do
+      medicine = create(:medicine)
+      visit medicines_path
+      expect(page).to have_content medicine.name
+    end
+
+    context '薬のデータに画像がある場合' do
+      it '画像が表示されること' do
+        create(:medicine)
+        visit medicines_path
+        expect(page).to have_css '.medicines_list__item--image'
+      end
+    end
+
+    context '薬のデータに画像がない場合' do
+      it '画像が表示されないこと' do
+        create(:medicine, image: nil)
+        visit medicines_path
+        expect(page).to have_css '.medicines_list__item--no_image'
+      end
+    end
+
+    context '薬のデータに参考サイトがある場合' do
+      it 'サイトへのリンクが表示されること' do
+        create(:medicine)
+        visit medicines_path
+        expect(page).to have_link '参考サイト'
+      end
+    end
+
+    context '薬のデータに参考サイトがない場合' do
+      it 'リンクが表示されないこと' do
+        create(:medicine, url: 'NULL')
+        visit medicines_path
+        expect(page).to have_css '.medicines_list__item--no_url'
+      end
+    end
+  end
 end
