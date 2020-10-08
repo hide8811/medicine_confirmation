efca432d0b123ba88e13a13e6d50d615e9b5b9ef
hide8811 <ikuyedih18@gmail.com>
2020-10-08 10:39:30 +0900

    Add: [medicine] アップローダーとカラムの関連付け

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -3,4 +3,6 @@ class Medicine < ApplicationRecord
   has_many :dosing_times, through: :medicine_dosing_times
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  mount_uploader :image, MedicineImageUploader
 end
