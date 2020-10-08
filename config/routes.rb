b4da4762325a20718be245cf5797696025c75ac8
hide8811 <ikuyedih18@gmail.com>
2020-10-08 10:33:34 +0900

    Add: [medicine] 新規登録のためのcreateアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'notes#index'
   resources :care_receivers, only: %i[new create show]
-  resources :medicines, only: :index
+  resources :medicines, only: %i[index create]
 end
