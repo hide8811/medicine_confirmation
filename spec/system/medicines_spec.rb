4572eaf8a4f2aacc74ab867038b335cc536da93d
hide8811 <ikuyedih18@gmail.com>
2020-10-08 10:25:20 +0900

    Add: [medicine] 新規登録のテストを追加

--- spec/system/medicines_spec.rb
+++ spec/system/medicines_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe 'Medicines', type: :system do
 
     context '薬のデータに画像がない場合' do
       it '画像が表示されないこと' do
-        create(:medicine, image: nil)
+        create(:medicine, image: '')
         visit medicines_path
         expect(page).to have_css '.medicines_list__item--no_image'
       end
@@ -60,9 +60,81 @@ RSpec.describe 'Medicines', type: :system do
 
     context '薬のデータに参考サイトがない場合' do
       it 'リンクが表示されないこと' do
-        create(:medicine, url: 'NULL')
+        create(:medicine, url: '')
         visit medicines_path
         expect(page).to have_css '.medicines_list__item--no_url'
+      end
+    end
+  end
+
+  describe '新規登録' do
+    let(:name) { Faker::Lorem.word }
+    let(:image) { 'medicine-test_image.png' }
+    let(:image_path) { "#{Rails.root}/spec/factories/image/#{image}" }
+    let(:url) { Faker::Internet.url }
+    before do
+      visit medicines_path
+    end
+
+    context '全ての値が入力されている場合' do
+      before do
+        fill_in 'medicine[name]', with: name
+        attach_file 'medicine[image]', image_path
+        fill_in 'medicine[url]', with: url
+        click_on '登録'
+      end
+
+      it { expect(page).to have_selector 'li', text: name }
+
+      it { expect(page).to have_selector "img[src$='#{image}']" }
+
+      it { expect(page).to have_link '参考サイト', href: url }
+    end
+
+    context 'input: name' do
+      context '空の場合' do
+        before do
+          fill_in 'medicine[name]', with: ''
+          attach_file 'medicine[image]', image_path
+          fill_in 'medicine[url]', with: url
+          click_on '登録'
+        end
+
+        it { expect(page).not_to have_css 'li' }
+      end
+    end
+
+    context 'input: image' do
+      context '空の場合' do
+        before do
+          fill_in 'medicine[name]', with: name
+          # 画像は空
+          fill_in 'medicine[url]', with: url
+          click_on '登録'
+        end
+
+        it { expect(page).to have_selector '.medicines_list__item--name', text: name }
+
+        it { expect(page).to have_css '.medicines_list__item--no_image' }
+
+        it { expect(page).to have_link '参考サイト', href: url }
+      end
+    end
+
+    context 'input: url' do
+      context '空の場合' do
+        before do
+          fill_in 'medicine[name]', with: name
+          attach_file 'medicine[image]', image_path
+          fill_in 'medicine[url]', with: ''
+          click_on '登録'
+        end
+
+        it { expect(page).to have_selector 'li', text: name }
+
+        it { expect(page).to have_selector "img[src$='#{image}']" }
+
+        it { expect(page).to have_css '.medicines_list__item--no_url' }
       end
     end
   end
