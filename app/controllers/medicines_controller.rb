8154f44a2b8760741c55ed760d852a6f3d173ff5
hide8811 <ikuyedih18@gmail.com>
2021-02-21 15:26:49 +0900

    Fix: [medicine] サイドボタンの修正

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -1,5 +1,6 @@
 class MedicinesController < ApplicationController
   def index
+    @care_receiver_id = care_receiver_id_params
     @medicines = Medicine.all
     @new_medicine = Medicine.new
   end
@@ -7,9 +8,9 @@ class MedicinesController < ApplicationController
   def create
     @new_medicine = Medicine.new(medicine_params)
     if @new_medicine.save
-      redirect_to medicines_path
+      redirect_to action: :index, care_receiver_id: params[:medicine][:care_receiver_id]
     else
-      render :index
+      render action: :index
     end
   end
 
@@ -17,5 +18,15 @@ class MedicinesController < ApplicationController
 
   def medicine_params
     params.require(:medicine).permit(:name, :image, :url)
+  end
+
+  def care_receiver_id_params
+    latest_route = Rails.application.routes.recognize_path(request.referer)
+
+    return nil if latest_route[:controller] == 'care_receivers'
+
+    return latest_route[:care_receiver_id] if latest_route[:controller] == 'dosing_times'
+
+    return params[:care_receiver_id] if latest_route[:controller] == 'medicines'
   end
 end
