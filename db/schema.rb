b1aa9ae04da1183135342e0e5a4eb22d53bf912d
hide8811 <ikuyedih18@gmail.com>
2021-06-21 23:39:33 +0900

    Improve: Railsのバージョンアップ
    bundle installができなくなったため。(mimemagicがなくなったから？)

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_02_23_132035) do
 
-  create_table "care_receivers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "care_receivers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_132035) do
     t.index ["discarded_at"], name: "index_care_receivers_on_discarded_at"
   end
 
-  create_table "dosing_times", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "dosing_times", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.time "time", null: false
     t.integer "timeframe_id", limit: 2, null: false
     t.bigint "care_receiver_id", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_132035) do
     t.index ["timeframe_id", "care_receiver_id", "discarded_at"], name: "index_dosing_time_timeframe_care_receiver_discard", unique: true
   end
 
-  create_table "medicine_dosing_times", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "medicine_dosing_times", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "medicine_id", null: false
     t.bigint "dosing_time_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_132035) do
     t.index ["medicine_id"], name: "index_medicine_dosing_times_on_medicine_id"
   end
 
-  create_table "medicines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "medicines", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "image"
     t.string "url"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_132035) do
     t.index ["name", "discarded_at"], name: "index_medicines_on_name_and_discarded_at", unique: true
   end
 
-  create_table "takes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "takes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.boolean "execute", default: false, null: false
     t.string "dosing_timeframe", null: false
     t.time "dosing_time", null: false
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_132035) do
     t.index ["user_id"], name: "index_takes_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "employee_id", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
